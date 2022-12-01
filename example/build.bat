ae9d2305b20b7e579e8da4f65bdc2a028b54bab4
Mark Jansen <mark.jansen@reactos.org>
2022-12-02 00:53:27 +0100

    bat

--- example/build.bat
+++ example/build.bat
@@ -1,5 +1,17 @@
 @echo off
 
+if not defined VCPKG_INSTALLED_DIR goto missing_vars
+if not defined VCPKG_DEFAULT_TRIPLET goto missing_vars
+
+
+
 echo Using:
 echo VCPKG_INSTALLED_DIR=%VCPKG_INSTALLED_DIR%
 echo VCPKG_DEFAULT_TRIPLET=%VCPKG_DEFAULT_TRIPLET%
+goto exit
+
+:missing_vars
+echo Please define VCPKG_INSTALLED_DIR and VCPKG_DEFAULT_TRIPLET before building!
+exit 1
+
+:exit
